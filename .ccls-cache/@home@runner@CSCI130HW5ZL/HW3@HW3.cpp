60febaefe4659cd26750b6a2656f9fa8504e46e8
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-09-26 08:39:49 +0000

    I got it! I think...

--- .ccls-cache/@home@runner@CSCI130HW5ZL/HW3@HW3.cpp
+++ .ccls-cache/@home@runner@CSCI130HW5ZL/HW3@HW3.cpp
@@ -1,39 +1,40 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
 #include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
 
 using namespace std;
 // WCI = K1 + 0.6125 Ta - K2*Ws^0.16 + K3TaWS^0.16
 // Ws = WCI
-int main()
-{
-double Ta; // Measured Temp
-double Ws; // Wind Speed
-double WCI; //Caculated Wind Speedd Index
-double K1 = 13.12; //First conversion Factor
-double K2 = 11.37; //Second Conversion Factor
-double K3 = 0.3965; // Third Conversion Factor
+int main() {
+  double Ta;          // Measured Temp
+  double Ws;          // Wind Speed
+  double WCI;         // Caculated Wind Speedd Index
+  double K1 = 13.12;  // First conversion Factor
+  double K2 = 11.37;  // Second Conversion Factor
+  double K3 = 0.3965; // Third Conversion Factor
 
-cout << "Enter Temp" << endl;
-cin >> Ta;
-cout << "Enter Wind Speed" << endl;
-cin >> Ws;
-
+Ws = 5;
   
-for (int i = 0; i < 3; i++)
-{
-WCI = ((K1 + 0.6125 * Ta) - (K2 * pow(Ws, 0.16) + (K3 * Ta * pow(Ws, 0.16))));
-Ta = Ta + 4;
-  cout << "Temp = " << Ta << " Wind = " << Ws << " WCI = " << WCI << endl;
-  cout << endl;
-
-  
-  }
-  }
+  for (int i = 0; i < 4; i++) // Start of outer loop
+  {
+    Ta = 2;
+   // Ws = 5;
+    for (int j = 0; j < 3; j++) // Start of Inner Loop
+    {
+      WCI = ((K1 + 0.6125 * Ta) - (K2 * pow(Ws, 0.16) + (K3 * Ta * pow(Ws, 0.16))));
+      cout << "Temp = " << Ta << endl;
+      cout << "Wind = " << Ws << endl;
+      cout << "WCI = " << WCI << endl;
+      cout << endl;
+      Ta = Ta + 4;
+    } // End of inner loop
+    Ws = Ws + 2;
+  } // End of outer loop
+}
 /*
-for (int j = i; j < 4; j++) 
+for (int j = i; j < 4; j++)
 Ws = Ws +2;
 
 
@@ -42,7 +43,6 @@ Ws = Ws +2;
 
 
 //*/
-
 
 /*
 cout << "Temp: " << "Wind Speed: " << "WCI:"<< endl;
